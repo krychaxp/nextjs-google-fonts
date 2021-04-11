3b42e51a1af0925d02621af9b80690d6b6cd3afe
Krychaxp <krychaxp@op.pl>
2021-04-11 14:58:02 +0200

    remove example

--- example/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Head from "next/head";
-import { GoogleFonts } from "nextjs-google-fonts/GoogleFonts";
-
-export default function index() {
-  return <Head>{GoogleFonts()}</Head>;
-}
